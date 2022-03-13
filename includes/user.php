458423f1f82fec665041e380813883d933814219
jorafael3 <jalvaradoe3@gmail.com>
2022-03-12 22:17:50 -0500

    asd

--- includes/user.php
+++ includes/user.php
@@ -17,7 +17,17 @@ class User extends Model
                 return "ok";
 
             }
-
+            // echo $_SERVER['SERVER_ADDR']."<br/>"; //Imprime la IP del servidor
+            // echo $_SERVER['SERVER_NAME']."<br/>"; //Imprime el nombre del servidor
+            // echo $_SERVER['SERVER_SOFTWARE']."<br/>"; //Imprime el software que usa el servidor
+            // echo $_SERVER['SERVER_PROTOCOL']."<br/>"; //Imprime el protocolo usado
+            // echo $_SERVER['REQUEST_METHOD']."<br/>"; //Imprime el método de petición empleado
+            // echo $_SERVER['REQUEST_TIME']."<br/>";  //Imprime el tiempo de respuesta
+            // echo $_SERVER['HTTP_USER_AGENT']."<br/>"; /*Imprime la información de S.O y navegador del cliente*/
+            // echo $_SERVER["REMOTE_ADDR"]."<br/>";  //Imprime la dirección IP del cliente
+            // /*Imprime puerto empleado por la máquina del usuario para comunicarse con el servidor web. */
+            // echo $_SERVER["REMOTE_PORT"]."<br/>";
+            
             // $usu = "";
             // $tipo = "";
             // $Nivel = "";
