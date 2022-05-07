994694103829b58ec5d84b57be2e0fafe8c49e15
jorafael3 <jalvaradoe3@gmail.com>
2022-05-07 18:29:14 -0500

    sd

--- views/sidebar.php
+++ views/sidebar.php
@@ -60,49 +60,6 @@ $TIPOUS_ID = $_SESSION["TIPOUS_ID"];
                         <span class="menu-title">Poa</span>
                     </a>
                 </div>
-                <div class="menu-item">
-                    <a class="menu-link" href="<?php echo constant("URL") ?>matrizestrategica/Matriz_Estrategica">
-                        <span class="menu-icon">
-                            <i class="fa fa-home"></i>
-                        </span>
-                        <span class="menu-title">Matriz Estrategica</span>
-                    </a>
-                </div>
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                    <span class="menu-link">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotune/communication/com013.svg-->
-                            <span class="svg-icon svg-icon-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path d="M6.28548 15.0861C7.34369 13.1814 9.35142 12 11.5304 12H12.4696C14.6486 12 16.6563 13.1814 17.7145 15.0861L19.3493 18.0287C20.0899 19.3618 19.1259 21 17.601 21H6.39903C4.87406 21 3.91012 19.3618 4.65071 18.0287L6.28548 15.0861Z" fill="black" />
-                                    <rect opacity="0.3" x="8" y="3" width="8" height="8" rx="4" fill="black" />
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
-                        </span>
-                        <span class="menu-title">Cuenta</span>
-                        <span class="menu-arrow"></span>
-                    </span>
-                    <div class="menu-sub menu-sub-accordion menu-active-bg">
-                        <div class="menu-item">
-                            <a class="menu-link" href="<?php echo constant("URL") ?>cuenta/Perfil_usuario">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Perfil</span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link" href="<?php echo constant("URL") ?>cuenta/Ajustes_usuario">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Ajustes</span>
-                            </a>
-                        </div>
-
-                    </div>
-                </div>
 
 
                 <?php
@@ -111,6 +68,15 @@ $TIPOUS_ID = $_SESSION["TIPOUS_ID"];
                 } else {
 
                 ?>
+
+                    <div class="menu-item">
+                        <a class="menu-link" href="<?php echo constant("URL") ?>matrizestrategica/Matriz_Estrategica">
+                            <span class="menu-icon">
+                                <i class="fa fa-home"></i>
+                            </span>
+                            <span class="menu-title">Matriz Estrategica</span>
+                        </a>
+                    </div>
 
                     <div data-kt-menu-trigger="click" class="menu-item menu-accordion mb-1">
                         <span class="menu-link">
@@ -227,11 +193,49 @@ $TIPOUS_ID = $_SESSION["TIPOUS_ID"];
                         </div> -->
                         </div>
                     </div>
+
+
                 <?php
 
                 }
 
                 ?>
+
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                    <span class="menu-link">
+                        <span class="menu-icon">
+                            <!--begin::Svg Icon | path: icons/duotune/communication/com013.svg-->
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M6.28548 15.0861C7.34369 13.1814 9.35142 12 11.5304 12H12.4696C14.6486 12 16.6563 13.1814 17.7145 15.0861L19.3493 18.0287C20.0899 19.3618 19.1259 21 17.601 21H6.39903C4.87406 21 3.91012 19.3618 4.65071 18.0287L6.28548 15.0861Z" fill="black" />
+                                    <rect opacity="0.3" x="8" y="3" width="8" height="8" rx="4" fill="black" />
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->
+                        </span>
+                        <span class="menu-title">Cuenta</span>
+                        <span class="menu-arrow"></span>
+                    </span>
+                    <div class="menu-sub menu-sub-accordion menu-active-bg">
+                        <div class="menu-item">
+                            <a class="menu-link" href="<?php echo constant("URL") ?>cuenta/Perfil_usuario">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Perfil</span>
+                            </a>
+                        </div>
+                        <div class="menu-item">
+                            <a class="menu-link" href="<?php echo constant("URL") ?>cuenta/Ajustes_usuario">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Ajustes</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
 
 
 
