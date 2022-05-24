eff9d20700d17f24f542ef6c02359e217cee0a7e
jorafael3 <jalvaradoe3@gmail.com>
2022-05-24 15:17:21 -0500

    <z

--- views/footer.php
+++ views/footer.php
@@ -9,20 +9,19 @@
                            <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
                                <div class="text-dark order-2 order-md-1">
                                    <span class="text-muted fw-bold me-1">2022©</span>
-                                   <img  src="<?php echo constant("URL") ?>public/assets/images/softlogo.PNG" alt="logo">
                                </div>
 
-                               <!-- <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+                               <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
                                    <li class="menu-item">
-                                       <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+                                   <img  src="<?php echo constant("URL") ?>public/assets/images/softlogo.PNG" alt="logo">
                                    </li>
-                                   <li class="menu-item">
+                                   <!-- <li class="menu-item">
                                        <a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
                                    </li>
                                    <li class="menu-item">
                                        <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-                                   </li>
-                               </ul> -->
+                                   </li> -->
+                               </ul>
                            </div>
                        </div>
                        <!--end::Footer-->
