8a5bd92190cc3b19aeb4a75cff329a84ec569fd4
jorafael3 <jalvaradoe3@gmail.com>
2022-05-24 15:09:29 -0500

    df

--- views/footer.php
+++ views/footer.php
@@ -9,7 +9,7 @@
                            <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
                                <div class="text-dark order-2 order-md-1">
                                    <span class="text-muted fw-bold me-1">2022©</span>
-                                   <img  src="<?php echo constant("URL") ?>public/assets/images/softlogo.png" alt="">
+                                   <img  src="<?php echo constant("URL") ?>public/assets/images/softlogo.PNG" alt="">
                                </div>
 
                                <!-- <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
