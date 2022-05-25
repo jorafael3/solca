84666e7af8a886c629d117a09626ecf1eddc2ce1
jorafael3 <jalvaradoe3@gmail.com>
2022-05-25 10:46:56 -0500

    asd

--- views/login/login.php
+++ views/login/login.php
@@ -13,6 +13,7 @@
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
     <link href="<?php echo constant('URL') ?>public/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo constant('URL') ?>public/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo constant('URL') ?>public/assets/css/lg.css" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
 </head>
 <!--end::Head-->
@@ -22,23 +23,112 @@
     <!--begin::Main-->
     <!--begin::Root-->
     <div class="d-flex flex-column flex-root">
-        <!--begin::Authentication - Sign-in -->
-        <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url(assets/media/illustrations/sketchy-1/14.png">
-            <!--begin::Content-->
-            <div class="d-flex flex-center flex-column flex-column-fluid p-5 pb-lg-5 bg-light">
-                <!--begin::Logo-->
-                <a href="#" class="mb-5">
+        <!--begin::Authentication - Signup Free Trial-->
+        <div class="d-flex flex-column flex-xl-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="col-xl-5 col-md-12">
+                <!--begin::Content-->
+                <div class="d-flex align-items-start flex-column p-3 p-lg-5">
+                    <!-- 
+                    <h1 class="text-dark fs-2x mb-3">Welcome, Guest!</h1>
 
-                    <img alt="Logo" src="<?php echo constant('URL') ?>public/assets/images/log2.png" class="h-150px" />
-                </a>
-                <!--end::Logo-->
+                    <div class="fw-bold fs-4 text-gray-400 mb-10">Plan your blog post by choosing a topic creating
+                        <br />an outline and checking facts
+                    </div> -->
+
+                    <div class="col-12 p-3">
+                        <!--begin::Card-->
+                        <div class="card">
+                            <!--begin::Card body-->
+                            <div style="background-color: #27AE60;" class="card-body shadow-sm d-flex flex-center flex-column pt-5 p-3">
+                                <div class="symbol symbol-115px symbol-circle mb-1">
+                                    <i style="font-size: 45px;" class="fa fa-eye text-light"></i>
+                                </div>
+                                <a href="#" class="fs-1 text-light text-hover-light fw-bolder mb-0 p-1">Visión del futuro</a>
+                                <div class="d-flex flex-center flex-wrap">
+                                    <div class="border border-gray-700 border-dashed rounded min-w-80px py-3 px-4 mx-2 mb-3">
+                                        <div class="fs-5  text-light">
+                                            <span>
+                                                Nos vemos al 2022 reconocidos por el estado,
+                                                la sociedad ecuatoriana y los pacientes como un
+                                                instituto médico oncológico de vanguardia,
+                                                eficiente, con atencion sensible y servivios de excelencia.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--end::Card-->
+                    </div>
+
+                    <div class="col-12 p-3">
+                        <!--begin::Card-->
+                        <div class="card">
+                            <!--begin::Card body-->
+                            <div style="background-color: #2980B9;" class="card-body bg-primary shadow-sm d-flex flex-center flex-column pt-5 p-3">
+                                <div class="symbol symbol-115px symbol-circle mb-1">
+                                    <i style="font-size: 45px;" class="fa fa-users text-light"></i>
+                                </div>
+                                <a href="#" class="fs-1 text-light text-hover-light fw-bolder mb-0 p-1">Propósito</a>
+                                <div class="d-flex flex-center flex-wrap">
+                                    <div class="border border-gray-700 border-dashed rounded min-w-80px py-3 px-4 mx-2 mb-3">
+                                        <div class="fs-5  text-light">
+                                            <span>
+                                               Salvar la mayor cantidad de vidas.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--end::Card-->
+                    </div>
+
+                    <div class="col-12 p-3">
+                        <!--begin::Card-->
+                        <div class="card">
+                            <!--begin::Card body-->
+                            <div style="background-color: #27AE60;" class="card-body shadow-sm d-flex flex-center flex-column pt-5 p-3">
+                                <div class="symbol symbol-115px symbol-circle mb-1">
+                                    <i style="font-size: 45px;" class="fa fa-bullseye text-light"></i>
+                                </div>
+                                <a href="#" class="fs-1 text-light text-hover-light fw-bolder mb-0 p-1">Misión</a>
+                                <div class="d-flex flex-center flex-wrap">
+                                    <div class="border border-gray-700 border-dashed rounded min-w-80px py-3 px-4 mx-2 mb-3">
+                                        <div class="fs-5  text-light">
+                                            <span>
+                                            Prevenir y detectar precozmente el cáncer y atender a los pacientes oncológicos 
+                                            de manera oportuna y solicitaria con tecnoloía, tratamientos adecuados 
+                                            y transparencia de gestión.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--end::Card-->
+                    </div>
+
+
+
+                </div>
+                <!--end::Content-->
+            </div>
+            <!--begin::Aside-->
+            <!--begin::Content-->
+            <div class="col-xl-7 col-md-12 flex-row-fluid d-flex flex-center justfiy-content-xl-first p-10">
                 <!--begin::Wrapper-->
-                <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
+                <div class="d-flex flex-center p-15 shadow-sm bg-body rounded w-100 w-md-550px mx-auto ms-xl-20">
                     <!--begin::Form-->
                     <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="<?php echo constant('URL') ?>" method="POST">
                         <!--begin::Heading-->
                         <div class="text-center mb-10">
                             <!--begin::Title-->
+                            <a href="#" class="mb-5">
+
+                                <img alt="Logo" src="<?php echo constant('URL') ?>public/assets/images/log2.png" class="h-150px" />
+                            </a>
                             <h1 class="text-dark mb-3">Inicia Sesion</h1>
 
                         </div>
@@ -58,7 +148,7 @@
                         </div>
                         <!--end::Input group-->
                         <!--begin::Input group-->
-                        <div class="fv-row mb-10">
+                        <div class="fv-row mb-10" data-kt-password-meter="true">
                             <!--begin::Wrapper-->
                             <div class="d-flex flex-stack mb-2">
                                 <!--begin::Label-->
@@ -68,7 +158,13 @@
                             </div>
                             <!--end::Wrapper-->
                             <!--begin::Input-->
-                            <input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
+                            <div class="position-relative mb-3">
+                                <input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
+                                <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
+                                    <i class="bi bi-eye-slash fs-2"></i>
+                                    <i class="bi bi-eye fs-2 d-none"></i>
+                                </span>
+                            </div>
                             <!-- <div class="">
                                         <?php
                                         if (isset($errorlogin)) {
@@ -108,19 +204,13 @@
                 </div>
                 <!--end::Wrapper-->
             </div>
-            <!--end::Content-->
-            <!--begin::Footer-->
-            <!-- <div class="d-flex flex-center flex-column-auto p-10">
-					<div class="d-flex align-items-center fw-bold fs-6">
-						<a href="https://keenthemes.com" class="text-muted text-hover-primary px-2">About</a>
-						<a href="mailto:support@keenthemes.com" class="text-muted text-hover-primary px-2">Contact</a>
-						<a href="https://1.envato.market/EA4JP" class="text-muted text-hover-primary px-2">Contact Us</a>
-					</div>
-				</div> -->
-            <!--end::Footer-->
+            <!--end::Right Content-->
         </div>
-        <!--end::Authentication - Sign-in-->
+        <!--end::Authentication - Signup Free Trial-->
     </div>
+
+
+
     <!--end::Root-->
     <!--end::Main-->
     <!--begin::Javascript-->
@@ -133,7 +223,8 @@
 
 
     <script>
-        $( "#email" ).focus();
+        $("#email").focus();
+
         function reset() {
             window.location.href = "./resetpassword.php";
 
